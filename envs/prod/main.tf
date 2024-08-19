9b31635c454b88c147e1149180268e89fb35c2fa
wooji9 <woojungin99@naver.com>
2024-08-19 23:12:29 +0900

    ec2-nginx instead ecs - 1

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -7,11 +7,11 @@ module "network" {
   region_id                 = "ap-northeast-1"
   vpc_cidr_block            = "192.168.1.0/24"
   a_public_subnet_01_cidr_block  = "192.168.1.0/27"
-  c_public_subnet_01_cidr_block  = "192.168.1.32/27"
+  #c_public_subnet_01_cidr_block  = "192.168.1.32/27"
   a_private_subnet_01_cidr_block = "192.168.1.64/27"
-  c_private_subnet_01_cidr_block = "192.168.1.96/27"
+  #c_private_subnet_01_cidr_block = "192.168.1.96/27"
   a_private_subnet_02_cidr_block = "192.168.1.128/27"
-  c_private_subnet_02_cidr_block = "192.168.1.160/27"
+  #c_private_subnet_02_cidr_block = "192.168.1.160/27"
   a_private_subnet_03_cidr_block = "192.168.1.192/27"
 
   tags = {
@@ -24,7 +24,7 @@ module "nginx" {
   
   env           = "prod"
   vpc_id        = module.network.vpc_id  # network 모듈의 output 참조
-  subnet_id     = module.network.a_private_subnet_03_id
+  subnet_id     = module.network.a_private_subnet_02_id
   ami_id        = "ami-0091f05e4b8ee6709" #region마다 ami id 다름
   instance_type = "t2.micro"
   instance_name = "nginx"
@@ -39,20 +39,13 @@ module "bastion" {
   
   env           = "prod"
   vpc_id        = module.network.vpc_id  # network 모듈의 output 참조
-  subnet_id     = module.network.a_public_subnet_01_id
+  subnet_id     = module.network.a_private_subnet_03_id
   ami_id        = "ami-0091f05e4b8ee6709" #region마다 ami id 다름
   instance_type = "t2.micro"
   instance_name = "bastion"
-  key_name      = "bastion-key" #"module.bastion.key_pair_name"
-  
+
   tags = {
     Environment = "prod"
   }
 
-  # 사용자 데이터 스크립트에서 환경변수로 저장된 ssh 키 참조
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "${BASTION_PRIVATE_KEY}" > /home/ec2-user/.ssh/id_rsa
-    chmod 400 /home/ec2-user/.ssh/id_rsa
-  EOF
 }
