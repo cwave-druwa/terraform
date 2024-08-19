80e860cdaf401395041c9610f55c5a17580213e7
wooji9 <woojungin99@naver.com>
2024-08-19 23:52:44 +0900

    ec2-nginx instead ecs - 7

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -18,21 +18,6 @@ output "a_private_subnet_02_id" {
   value       = module.network.a_private_subnet_02_id
 }
 
-output "c_public_subnet_01_id" {
-  description = "The ID of the public subnet"
-  value       = module.network.c_public_subnet_01_id
-}
-
-output "c_private_subnet_01_id" {
-  description = "The ID of the first private subnet"
-  value       = module.network.c_private_subnet_01_id
-}
-
-output "c_private_subnet_02_id" {
-  description = "The ID of the second private subnet"
-  value       = module.network.c_private_subnet_02_id
-}
-
 output "nginx_server_instance_id" {
   description = "The ID of the Nginx instance"
   value       = module.nginx.nginx_server_instance_id
@@ -41,9 +26,4 @@ output "nginx_server_instance_id" {
 output "bastion_server_instance_id" {
   description = "The ID of the bastion instance"
   value       = module.bastion.bastion_server_instance_id
-}
-
-output "private_key_pem" {
-  value     = module.bastion.private_key_pem
-  sensitive = true
 }
