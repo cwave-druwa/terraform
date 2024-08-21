75deb1a903d779306d0fed2775fde54380b2a5aa
root <root@KHP01>
2024-08-21 14:34:04 +0900

    ect test 5

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -25,6 +25,8 @@ resource "aws_lb_target_group" "nginx_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.network.vpc_id
+  
+  target_type = "ip"  # IP 기반 타겟 그룹으로 설정
 }
 
 resource "aws_lb_listener" "http" {
