27b13a78142a8aafff322ad0dddc5b0bcf780efb
root <root@KHP01>
2024-08-24 17:14:08 +0900

    ecr 연결 5

--- modules/ecs/ecs_main.tf
+++ modules/ecs/ecs_main.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "nginx" {
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 8080
+          #hostPort      = 8080
         }
       ]
       #로그 설정 추가
@@ -58,7 +58,7 @@ resource "aws_ecs_task_definition" "olive" {
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 8080
+          #hostPort      = 8080
         }
       ]
       #로그 설정 추가
